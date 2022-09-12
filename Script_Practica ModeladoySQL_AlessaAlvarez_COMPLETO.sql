6f4329a5a656d6192d6cce2ca0d0ea087b047d12
alessaag <alessaag@gmail.com>
2022-09-11 21:21:09 -0500

    Add files via upload

---
+++ Script_Practica ModeladoySQL_AlessaAlvarez_COMPLETO.sql
@@ -0,0 +1,15 @@
+/*
+ * 
+ * Consulta SQL
+ * 
+ */
+
+select c.dt_compra , d."name",b."name", a.matricula, e."name", a.num_kilometros,f."name", a.num_poliza
+from alessamaster.vehiculos_kc a 
+inner join alessamaster.marcas b on a.marca = b.marca
+inner join alessamaster.hist_revisiones c on a.idvehiculo = c.id_vehiculo 
+inner join alessamaster.grupos_empresariales d on b.grupo_empresarial = d.grupo_empresarial 
+inner join alessamaster.colors e on a.color = e.color 
+inner join alessamaster.aseguradoras f on a.aseguradora = f.aseguradora
+
+
